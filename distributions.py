4b17ef6f0e35dd4eefc96ea5f7612e797521a20e
Daniel Billotte <daniel@litfuze.com>
2025-04-25 16:44:20 -0700

    foo

--- distributions.py
+++ distributions.py
@@ -2,7 +2,10 @@ import math
 
 
 def binomial_pmf(n, p, k):
-    return (n-k)*p/((k+1)*(1-p))
+    if k < 0 or k > n:
+        return 0
+    comb = math.comb(n, k)  # or use factorial(n) / (factorial(k) * factorial(n-k))
+    return comb * (p ** k) * ((1 - p) ** (n - k))
 
 
 def poisson_pmf(k, lam):
